6cd3f466ca085562c9c722b7bfd272c387d7727a
Aldo <aeveraespinoza@mail.lipscomb.edu>
2024-06-02 14:24:59 -0500

    new project

---
+++ bittwiddling/twiddle.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello! This file has functions for playing around with bits.")
+	fmt.Println("Source: https://graphics.stanford.edu/~seander/bithacks.html#OperationCounting")
+	fmt.Println("This is more so for me to get used to Go and bits.")
+
+	fmt.Println()
+}
